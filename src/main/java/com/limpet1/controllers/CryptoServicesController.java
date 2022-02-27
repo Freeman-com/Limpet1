40933583c0f06dbe1852a2cf9a7044a7eb8d98cc
Freeman-com <5244934@gmail.com>
2022-02-27 09:31:39 +0300

    cryptoservices page ended

--- src/main/java/com/limpet1/controllers/CryptoServicesController.java
+++ src/main/java/com/limpet1/controllers/CryptoServicesController.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -26,13 +25,17 @@ public class CryptoServicesController {
     private static final String CRYPTO = "cryptoservices";
     private static final String TRANSACTION = "transaction";
     private final UserRepositoryJPA userRepositoryJPA;
+    private final AscendexRepository ascendexRepository;
+    private final AscendexRestControllerV3 ascendexRestControllerV3;
     private final BinanceRestControllerV2 binanceRestControllerV2;
     public final CurrentPrice currentPrice;
 
     public CryptoServicesController(UserRepositoryJPA userRepositoryJPA, AscendexRepository ascendexRepository,
-                                    BinanceRestControllerV2 binanceRestControllerV2,
+                                    AscendexRepository ascendexRepository1, AscendexRestControllerV3 ascendexRestControllerV31, BinanceRestControllerV2 binanceRestControllerV2,
                                     AscendexRestControllerV3 ascendexRestControllerV3, CurrentPrice currentPrice) {
         this.userRepositoryJPA = userRepositoryJPA;
+        this.ascendexRepository = ascendexRepository1;
+        this.ascendexRestControllerV3 = ascendexRestControllerV31;
         this.binanceRestControllerV2 = binanceRestControllerV2;
         this.currentPrice = currentPrice;
     }
@@ -42,24 +45,49 @@ public class CryptoServicesController {
 
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         XUser xUser = userRepositoryJPA.findByEmail(user.getUsername());
-        List<String> list = Arrays.asList("Name", "Rated", "Type");
-        List<Map<String, Object>> columns = new ArrayList<>();
         String email = xUser.getEmail();
+        List<String> list = Arrays.asList("Ticker", "Market Price", "Quantity", "NetCost", "Total in USD");
+        List<Map<String, Object>> columns = new ArrayList<>();
 
+        /* ---  Exchange rates --- */
+
+
+        var keyList = ascendexRepository.findByUsersId(xUser.getId());
         var a = binanceRestControllerV2.coinInfo1(email);
+        var b = ascendexRestControllerV3.accountInfo(email);
 
+
+        Double value = null;
         for (var i : a.entrySet()) {
             double x = currentPrice.averagePriceImpl(i.getKey());
             double total = i.getValue() * x;
 
-            columns.add(Map.of("Name", i.getKey(), "Rated", i.getValue() * x + " $", "Type", i.getValue()));
+            columns.add(Map.of("Ticker", i.getKey(), "Market Price", i.getValue() * x + " $", "Quantity",
+
+                    i.getValue() + "  " + i.getKey(), "NetCost", "0" + " $", "Total in USD",
+
+                    total + " $"));
+
+            value = total++;
         }
 
-        Double value = 1250.001;
+
+        for (var i : b.entrySet()) {
+            double x = currentPrice.averagePriceImpl(i.getKey());
+            double total = i.getValue() * x;
+
+            columns.add(Map.of("Ticker", i.getKey(), "Market Price", i.getValue() * x + " $", "Quantity",
+
+                    i.getValue() + "  " + i.getKey(), "NetCost", "0" + " $", "Total in USD",
+
+                    total + " $"));
+            value = total++;
+        }
+
+        model.addAttribute("value", value);
+        model.addAttribute("xUser", xUser);
         model.addAttribute("list", list);
         model.addAttribute("columns", columns);
-        model.addAttribute("xUser", xUser);
-        model.addAttribute("value", value);
 
         return CRYPTO;
     }
